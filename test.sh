31ab7f8d59bc7b358ef354e01aca838b1d960e0a
honglin-chen <hc-tpuv4-32-3>
2023-02-15 17:17:01 -0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+TPU_NAME=hc-tpuv4-32-1
+ZONE=us-central2-b
+
 gcloud alpha compute tpus tpu-vm ssh $TPU_NAME --zone $ZONE --worker all --command "
     cd ~/tpu_debug;
     PJRT_DEVICE=TPU MASTER_ADDR=localhost MASTER_PORT=12500 python3 test_script.py
