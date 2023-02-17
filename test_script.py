0d435ec4a5ed9053b0f0fe26393aff3b154aeb05
Siyuan Liu <lsiyuan@google.com>
2023-02-17 01:17:07 +0000

    update pjrt init

--- test_script.py
+++ test_script.py
@@ -33,7 +33,7 @@ def get_args():
 
 def main(rank, args):
     print('In main function')
-    dist.init_process_group('xla', world_size=xm.xrt_world_size(), rank=xm.get_ordinal())
+    dist.init_process_group('xla', init_method='pjrt://', world_size=xm.xrt_world_size(), rank=xm.get_ordinal())
     device = xm.xla_device()
     print('End of main function')
 
